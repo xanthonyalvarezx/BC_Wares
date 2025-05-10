220bd8041fa32f58f4bc2e15f94384050672dd24
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2025-05-10 14:49:45 -0400

    added pages

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BC Wares | {$title}</title>
+</head>
+
+<body>
+    <main>
+        <nav>
+            nav
+        </nav>
+    </main>
+    {{ $slot }}
+    <footer>
+        footer
+    </footer>
+</body>
+
+</html>
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,4 @@
+<x-main-layout title="HOME">
+    <h1>Landing</h1>
+</x-main-layout>
+git
