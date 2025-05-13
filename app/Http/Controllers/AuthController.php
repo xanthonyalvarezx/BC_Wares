b140be748578e6d2cc42b5fae923a4fbae7b7fce
Anthony Alvarez <anthonyalvarez@Mac.lan>
2025-05-13 07:55:32 -0400

    started auth controller setup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        $validatedData = $request->validate([
+
+        ]);
+
+    }
+    public function registerVendor(Request $request){
+        $validatedData = $request->validate([
+
+        ]);
+
+    }
 }
